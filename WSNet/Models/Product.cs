2f6906b74cf301c69b3252b543373ea4b3c1c0a0
CARLOS\carlos <crosales@we-are-around.com>
2017-08-23 18:48:43 -0300

    Initial commit

---
+++ WSNet/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSNet.Models
+{
+    public class Product
+    {
+        public int      ID         { get; set; }
+        public String   Name       { get; set; }
+        public int      CategoryID { get; set; }
+        public decimal? UnitPrice  { get; set; }
+        public int      UnitsStock { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ WSNet/Models/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+//se deja de usar porque se colocan en IdentityModels que es donde
+//namespace WSNet.Models
+//{
+//    public class StoreContext : DbContext
+//    {
+//            public DbSet<Product> Products { get; set; }
+//            public DbSet<Category> Categories { get; set; }
+//    }
+//}
